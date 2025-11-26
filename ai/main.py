e97a0907940f6e713f9d8e8cda90aa7e71859e06
Mseunghwan <minccc42710@gmail.com>
2025-11-27 01:28:07 +0900

    feat : langchain(gemini-2.5-flash) 실행 후 LLM 응답 확인 완료

--- ai/main.py
+++ ai/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
-# OpenAI 사용 위해 import
-from langchain_openai import ChatOpenAI
+# GEMINI 사용 위해 import
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 import os
+
+# 결과 예쁘게
+from langchain_core.output_parsers import StrOutputParser
+
 
 app = FastAPI(root_path="/ai")
 
@@ -14,8 +18,27 @@ class StoryRequest(BaseModel):
     topic: str
     words: list[str]
 
-# 3. LangChain 설정
-llm = ChatOpenAI(model="gpt-4o", temperature=0.7)
+# LangChain 설정, 우선 무료 버전인 gemini-1.5로 테스트
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash", 
+    temperature=0.7
+)
+
+# Prompt 설정
+prompt_template = PromptTemplate.from_template(
+    """
+    당신은 창의적인 유아용 동화 작가입니다. 
+    다음 조건에 맞춰 아이를 위한 짧고 재미있는 동화를 한국어로 지어주세요.
+    아이를 위한 동화이기 때문에, 희망적인 메시지가 담겨있고 해피엔딩으로 끝나야 합니다.
+    분량은 1문단 당 5 ~ 6 문장으로 구성되며, 총 2 ~ 3 문단이 나와야 합니다.
+    
+    - 대상 연령: {age}세
+    - 주제: {topic}
+    - 필수 포함 단어: {words}
+    
+    동화 내용:                                   
+    """
+                                               )
 
 @app.get("/")
 def read_root():
@@ -23,4 +46,16 @@ def read_root():
 
 @app.post("/generate-story")
 async def generate_story(req: StoryRequest):
-    return {"story": f"{req.age}세 아이를 위해 {req.topic} 주제로 동화를 만들 예정입니다."}
+    # 체인 연결
+    chain = prompt_template | llm | StrOutputParser()
+
+    try :
+        response = chain.invoke({
+            "age" : req.age,
+            "topic" : req.topic,
+            "words" : ", ".join(req.words)
+        })
+
+        return {"story" : response}
+    except Exception as e :
+        raise HTTPException(status_code=500, detail=str(e))
